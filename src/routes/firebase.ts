53f0eb9295720850453a71c22804bec72db5c134
everyoon <jy.k7377@gmail.com>
2025-01-24 23:50:48 +0900

    project setting

---
+++ src/routes/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA_B2dt5m1PM-1ezzyHr1C1ydjZS1vS-00',
+  authDomain: 'twitter-b75e2.firebaseapp.com',
+  projectId: 'twitter-b75e2',
+  storageBucket: 'twitter-b75e2.firebasestorage.app',
+  messagingSenderId: '229923891399',
+  appId: '1:229923891399:web:1d2fab38092290809d76e2',
+};
+
+const app = initializeApp(firebaseConfig);
