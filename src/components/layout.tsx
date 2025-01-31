c4aa9fe00d423cc4ab023bd58a9d575c9f831e38
everyoon <jy.k7377@gmail.com>
2025-01-31 23:56:03 +0900

    Uploading Img

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,79 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { auth } from '../routes/firebase';
 
+const Wrapper = styled.div`
+  display: grid;
+  gap: 50px;
+  grid-template-columns: 1fr 4fr;
+  padding: 50px 0;
+  width: 100%;
+  max-width: 860px;
+  height: 100%;
+`;
+const Menu = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+`;
+const MenuItem = styled.div`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 2px solid #fff;
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+  svg {
+    width: 30px;
+    fill: #fff;
+  }
+  &.log-out {
+    border-color: #ff6347;
+    svg {
+      fill: #ff6347;
+    }
+  }
+`;
 export default function Layout() {
+  const navigate = useNavigate();
+  const onLogOut = async () => {
+    const ok = confirm('로그아웃을 하시겠습니까?');
+    if (ok) {
+      await auth.signOut();
+      navigate('/login');
+    }
+  };
   return (
-    <>
-      <h2>layout</h2>
+    <Wrapper>
+      <Menu>
+        <Link to="/">
+          <MenuItem>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
+            </svg>
+          </MenuItem>
+        </Link>
+        <Link to="/profile">
+          <MenuItem>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+            </svg>
+          </MenuItem>
+        </Link>
+        <MenuItem onClick={onLogOut} className="log-out">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+            <path
+              fillRule="evenodd"
+              d="M7.5 3.75A1.5 1.5 0 0 0 6 5.25v13.5a1.5 1.5 0 0 0 1.5 1.5h6a1.5 1.5 0 0 0 1.5-1.5V15a.75.75 0 0 1 1.5 0v3.75a3 3 0 0 1-3 3h-6a3 3 0 0 1-3-3V5.25a3 3 0 0 1 3-3h6a3 3 0 0 1 3 3V9A.75.75 0 0 1 15 9V5.25a1.5 1.5 0 0 0-1.5-1.5h-6Zm5.03 4.72a.75.75 0 0 1 0 1.06l-1.72 1.72h10.94a.75.75 0 0 1 0 1.5H10.81l1.72 1.72a.75.75 0 1 1-1.06 1.06l-3-3a.75.75 0 0 1 0-1.06l3-3a.75.75 0 0 1 1.06 0Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </MenuItem>
+      </Menu>
       <Outlet />
-    </>
+    </Wrapper>
   );
 }
